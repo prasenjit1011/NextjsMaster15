316dfbc6cfeeff8acfd67c0259724d88ce6b1e89
Prasenjit <prasenjit.aluni@gmail.com>
2025-05-15 22:53:10 +0530

    User Edit

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -6,7 +6,6 @@ import { DeleteButton } from './delete-btn'; // Client component for delete
 export default async function HomePage() {
   const users = await getData();
 
-  console.log('userlist : ', users)
 
   return (
     <div className="p-6">
